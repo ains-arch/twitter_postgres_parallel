0cea17e2f7265fd582edd9bbf211b49f919c8836
ains-arch <ainsleearchibald@gmail.com>
2024-04-11 21:14:53 -0700

    not yet

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,14 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time echo '$files' | parallel ./load_denormalized.sh
+# time echo '$files' | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:1447 --inputs "$file"
+# time echo "$files" | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:1447 --inputs "$file"
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1457/ --inputs $file
+# time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1457/ --inputs $file
